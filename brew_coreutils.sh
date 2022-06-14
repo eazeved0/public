5eed81c87395868b4433f5fb7bd1e50bb37a31a4
eazeved0 <eazevedo@bsd.com.br>
2022-06-14 03:35:30 -0300

    Create brew_coreutils.sh

---
+++ brew_coreutils.sh
@@ -0,0 +1,6 @@
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    brew update
+    brew reinstall coreutils -q
+    PATH="/opt/homebrew/opt/coreutils/libexec/gnubin:$PATH"
+    uid=$(gstat -c '%u' $home
+    gid=$(gstat -c '%g' $home)
